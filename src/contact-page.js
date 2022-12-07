81e763ec2212f5c80417f99118d1502fcc61fe28
Ioana Junc <ioanaj11@yahoo.com>
2022-12-07 14:09:49 +0200

    Added details on contact page

--- src/contact-page.js
+++ src/contact-page.js
@@ -8,28 +8,47 @@ function LoadContactPage(){
     const title=document.createElement('h2');
     title.textContent='Contact us at:';
 
-    mainDiv.appendChild(title);
-
     const contactMainDiv=document.createElement('div');
     contactMainDiv.classList.add('contactMainDiv');
 
+    contactMainDiv.appendChild(title);
+
     const contactDetailsDiv=document.createElement('div');
     contactDetailsDiv.classList.add('contactDetailsDiv');
+
+    let contactDetails=[{
+        icon: 'phone-call.png',
+        detail: '1234 567 980',
+        },
+        {icon:'maps-and-flags.png',
+        detail: 'Bulevardul Decebal, nr. 23, Oradea, Romania'},
+        {icon: 'email.png',
+        detail:'mywonderfulrestaurant@gmail.com'},
+        {icon:'timer.png',
+        detail:'Monday-Friday, 10:00-23:00; Saturday-Sunday 10:00-17:00'
+        }
+    ]
     
-    const phoneIcon=document.createElement('img');
-    phoneIcon.setAttribute('src','phone-call.png');
-    contactDetailsDiv.appendChild(phoneIcon);
+    for(let i=0; i<contactDetails.length; i++){
+            const icon=document.createElement('img');
+            icon.setAttribute('src', `${contactDetails[i].icon}`);
+            
+            contactDetailsDiv.appendChild(icon);
 
-    contactDetailsDiv.appendChild(phoneIcon);
-    
-    const phone=document.createElement('p');
-    phone.textContent='+40 123 456 789';
+            const detail=document.createElement('p');
+            detail.textContent=`${contactDetails[i].detail}`;
 
-    contactDetailsDiv.appendChild(phone);
-
+            contactDetailsDiv.appendChild(detail);
+        
+    }
+   
     contactMainDiv.appendChild(contactDetailsDiv);
 
     mainDiv.appendChild(contactMainDiv);
+
+
 }
+
+
 
 export{LoadContactPage};
